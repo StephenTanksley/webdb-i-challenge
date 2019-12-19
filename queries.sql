03972b6811b130b34337fb5fe1bed250ed5f862a
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-12-19 16:19:56 -0600

    Adding some of the updated SQL queries to the Queries file.

--- queries.sql
+++ queries.sql
@@ -1,16 +1,43 @@
 -- Database Queries
 
+
 -- Find all customers with postal code 1010
+
+SELECT * 
+FROM Customers 
+WHERE PostalCode = 1010;
 
 -- Find the phone number for the supplier with the id 11
 
+SELECT Phone 
+FROM Suppliers
+WHERE SupplierID = 11;
+
 -- List first 10 orders placed, sorted descending by the order date
+    -- Not quite sure how to get the first 10 orders placed.
+
+SELECT * FROM Orders
+ORDER BY OrderDate DESC
+LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
+    -- IN allows us to specify multiple values. Multiple conditions.
+
+SELECT *
+FROM Customers
+WHERE City IN('London', 'Madrid') 
+OR Country IN('Brazil');
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+UPDATE Customers
+WHERE CustomerName = 'The Shire'
+SET PostalCode = '11122';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
