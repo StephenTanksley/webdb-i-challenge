713152ad04f43894c08a02f40c4c97053996a892
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-12-26 10:57:11 -0600

    updating to reflect completed project.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./server.js');
 
+server.get('/', (req, res) => {
+  res.json({ message: "Welcome to the cars server."})
+})
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
--- queries.sql
+++ queries.sql
@@ -1,48 +1,48 @@
--- Database Queries
+-- -- Database Queries
 
 
--- Find all customers with postal code 1010
+-- -- Find all customers with postal code 1010
 
-SELECT * 
-FROM Customers 
-WHERE PostalCode = 1010;
+-- SELECT * 
+-- FROM Customers 
+-- WHERE PostalCode = 1010;
 
--- Find the phone number for the supplier with the id 11
+-- -- Find the phone number for the supplier with the id 11
 
-SELECT Phone 
-FROM Suppliers
-WHERE SupplierID = 11;
+-- SELECT Phone 
+-- FROM Suppliers
+-- WHERE SupplierID = 11;
 
--- List first 10 orders placed, sorted descending by the order date
-    -- Not quite sure how to get the first 10 orders placed.
+-- -- List first 10 orders placed, sorted descending by the order date
+--     -- Not quite sure how to get the first 10 orders placed.
 
-SELECT * FROM Orders
-ORDER BY OrderDate DESC
-LIMIT 10;
+-- SELECT * FROM Orders
+-- ORDER BY OrderDate DESC
+-- LIMIT 10;
 
--- Find all customers that live in London, Madrid, or Brazil
-    -- IN allows us to specify multiple values. Multiple conditions.
+-- -- Find all customers that live in London, Madrid, or Brazil
+--     -- IN allows us to specify multiple values. Multiple conditions.
 
-SELECT *
-FROM Customers
-WHERE City IN('London', 'Madrid') 
-OR Country IN('Brazil');
+-- SELECT *
+-- FROM Customers
+-- WHERE City IN('London', 'Madrid') 
+-- OR Country IN('Brazil');
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+-- -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
-INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country)
-VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
+-- INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country)
+-- VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
--- Update Bilbo Baggins record so that the postal code changes to "11122"
-    --need to set the action first before the specific target. 
+-- -- Update Bilbo Baggins record so that the postal code changes to "11122"
+--     --need to set the action first before the specific target. 
 
-UPDATE Customers
-SET PostalCode = '11122';
-WHERE CustomerName = 'The Shire'
+-- UPDATE Customers
+-- SET PostalCode = '11122';
+-- WHERE CustomerName = 'The Shire'
 
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+-- -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 
 
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+-- -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
 
